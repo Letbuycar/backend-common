f0c5f7d3d1f5f45f3443380250ff9b52b5e4c007
shevchenko126 <43893100+shevchenko126@users.noreply.github.com>
2024-11-28 15:54:34 +0200

    Create sqs.py

---
+++ letbuycar_backend_common/messaging/sqs.py
@@ -0,0 +1,47 @@
+import boto3
+from botocore.exceptions import BotoCoreError, ClientError
+import os
+
+
+class SQSService:
+    def __init__(self):
+        AWS_REGION_NAME = os.getenv('AWS_REGION_NAME')
+        AWS_SQS_QUEUE_URL = os.getenv('AWS_SQS_QUEUE_URL')
+        self.client = boto3.client(
+            "sqs",
+            region_name=AWS_REGION_NAME
+        )
+        self.sqs_url = AWS_SQS_QUEUE_URL
+
+    def send_message(self, message_body: str, attrs: dict = None) -> dict:
+        try:
+            response = self.client.send_message(
+                QueueUrl=self.queue_url,
+                MessageBody=message_body,
+                DelaySeconds=0,
+                MessageAttributes=attrs
+            )
+            return response
+        except (BotoCoreError, ClientError) as e:
+            raise RuntimeError(f"Ошибка отправки сообщения: {e}")
+
+    def receive_messages(self, max_messages: int = 1, wait_time: int = 10) -> list:
+        try:
+            response = self.client.receive_message(
+                QueueUrl=self.queue_url,
+                MaxNumberOfMessages=max_messages,
+                WaitTimeSeconds=wait_time,
+            )
+            return response.get("Messages", [])
+        except (BotoCoreError, ClientError) as e:
+            raise RuntimeError(f"Ошибка получения сообщений: {e}")
+
+    def delete_message(self, receipt_handle: str) -> None:
+        try:
+            self.client.delete_message(
+                QueueUrl=self.queue_url,
+                ReceiptHandle=receipt_handle,
+            )
+        except (BotoCoreError, ClientError) as e:
+            raise RuntimeError(f"Ошибка удаления сообщения: {e}")
+        
